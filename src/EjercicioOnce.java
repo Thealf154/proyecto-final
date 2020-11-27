01b0bd0af37ebf3e4ef331b9c408703c9eb5129e
Alfredo Vanegas <thealf154@gmail.com>
2020-11-27 13:26:25 -0600

    Arreglo de formato

--- src/EjercicioOnce.java
+++ src/EjercicioOnce.java
@@ -15,42 +15,41 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class EjercicioOnce{
+public class EjercicioOnce {
     private static Scanner entradas;
-    public static void main(String[] args) {
-        int[] numeros; //El arreglo de números naturales
-        int númeroSolicitado; //El número que el usuario desea ingresar al arreglo
-        int N; //La catidad de números en el arreglo de números
 
-            System.out.println("Ingrese la catidad de números a ingresar");
-            entradas = new Scanner(System.in);
-            N = entradas.nextInt();
+    public static void main(String[] args) {
+        int[] numeros; // El arreglo de números naturales
+        int númeroSolicitado; // El número que el usuario desea ingresar al arreglo
+        int N; // La catidad de números en el arreglo de números
+
+        System.out.println("Ingrese la catidad de números a ingresar");
+        entradas = new Scanner(System.in);
+        N = entradas.nextInt();
 
         numeros = new int[N];
 
-        //Ingresar todos los números al array
-        for(int i = 0; i < N; i++)
-        {
-            try
-            {
-                    System.out.println("Ingresa el número entero");
-                    númeroSolicitado = entradas.nextInt();
+        // Ingresar todos los números al array
+        for (int i = 0; i < N; i++) {
+            try {
+                System.out.println("Ingresa el número entero");
+                númeroSolicitado = entradas.nextInt();
 
                 numeros[i] = númeroSolicitado;
-            } catch (InputMismatchException e){
-                    System.out.println("No ingresaste un número entero.");
-                    System.out.println("Inténtalo otra vez");
+            } catch (InputMismatchException e) {
+                System.out.println("No ingresaste un número entero.");
+                System.out.println("Inténtalo otra vez");
 
-                //Se necesita limpiar el scanner porque seguirá dando excepciones, cuasando un cliclo infinito.
+                // Se necesita limpiar el scanner porque seguirá dando excepciones, cuasando un
+                // cliclo infinito.
                 entradas.next();
                 i--;
             }
         }
 
-        //Dar todos los números en reversa
+        // Dar todos los números en reversa
         System.out.println("\nEl arreglo en reversa:");
-        for(int j = --N; j != -1; j--)
-        {
+        for (int j = --N; j != -1; j--) {
             int númeroResultante = numeros[j];
             System.out.println(númeroResultante);
         }
