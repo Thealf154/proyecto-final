d731f065d61571740d0a4dfc78b1ac46e0e9fb25
Alfredo Vanegas <thealf154@gmail.com>
2020-11-28 00:49:53 -0600

    EjercioOcho hecho con éxito

---
+++ src/EjercicioOcho.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class EjercicioOcho {
+    private static Scanner entradas;
+
+    public static void main(String[] args) {
+        double angúlo;
+        double seno;
+
+        try {
+            System.out.println("Ingresa el ángulo que quieres");
+            entradas = new Scanner(System.in);
+            angúlo = entradas.nextDouble();
+
+            if (360 < angúlo || angúlo < -360) {
+                System.out.println("Ingrese un angúlo válido");
+                main(args);
+            }
+            else{
+                seno = Math.sin(Math.toRadians(angúlo));
+                System.out.printf("\nEl seno es: %.2f", seno);
+            }
+
+        } catch (InputMismatchException e) {
+                System.out.println("Ingrese un angúlo válido");
+            main(args);
+        }
+
+    }
+}
