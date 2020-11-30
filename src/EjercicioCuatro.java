58b5f93d0c3a5d3b8767e9c9e445a31968cfd143
Alfredo Vanegas <thealf154@gmail.com>
2020-11-30 14:24:13 -0600

    No estamos seguros de hacer esto

---
+++ src/EjercicioCuatro.java
@@ -0,0 +1,9 @@
+import java.util.LinkedHashMap;
+
+public class EjercicioCuatro {
+    public static void main(String[] args) {
+        String[] ciudades;
+        int[] tiendas;
+        int[] empleados;
+    }
+}
