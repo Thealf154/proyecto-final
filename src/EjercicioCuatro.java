b5934818afa30203a284406c226005d83bbdcd2f
Alfredo Vanegas <thealf154@gmail.com>
2020-12-01 12:26:28 -0600

    Added menu and deleted EjercicioCuatro

--- src/EjercicioCuatro.java
+++
@@ -1,9 +0,0 @@
-import java.util.LinkedHashMap;
-
-public class EjercicioCuatro {
-    public static void main(String[] args) {
-        String[] ciudades;
-        int[] tiendas;
-        int[] empleados;
-    }
-}
---
+++ src/Menu.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Menu {
+    private static Scanner entradas;
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Ingrese el número de algoritmo que quiere"); 
+        System.out.println("Algortimos disponibles: 5, 8, 11, 13, 14");
+        entradas = new Scanner(System.in);
+        n = entradas.nextInt();
+
+        switch (n) {
+            case 5:
+                EjercicioCinco.main(args);
+                break;
+            case 8:
+                EjercicioOcho.main(args);
+                break;
+            case 11:
+                EjercicioOnce.main(args);
+                break;
+            case 13:
+                try {
+                    EjercicioTrece.main(args);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case 14:
+                EjercicioCatorce.main(args);
+                break;
+
+            default:
+                System.out.println("Ingrese un número válido");
+        }
+    }
+}
